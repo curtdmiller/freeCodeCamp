df78c671f0490e09f0024f3b763396acf2097488
Curt D Miller <curtdmiller@gmail.com>
2017-02-28 16:41:58 -0500

    completed sum numbers in a range challenge

--- intermediateAlgorithmsJS/01-sumRange.js
+++ intermediateAlgorithmsJS/01-sumRange.js
@@ -7,14 +7,20 @@
  * The lowest number will not always come first.
  */
 
-
-
 function sumAll(arr) {
-    
-    return 1;
+    var max = arr.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+    var min = arr.reduce(function(a, b) {
+        return Math.min(a, b);
+    });
+    var acc = 0;
+    for (var i = min; i <= max; i++) {
+        acc += i;
+    }
+    return acc;
 }
 
-sumAll([1, 4]);
 sumAll([1, 4]); // should return 10.
 sumAll([4, 1]); // should return 10.
 sumAll([5, 10]); // should return 45.
