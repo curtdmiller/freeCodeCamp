e4b56b06eaa8e9ef7a8a9ca7806bf0523196d276
Curt D Miller <curtdmiller@gmail.com>
2017-03-02 13:28:23 -0500

    version with basic functionality and twitter button regenerated on each quote refresh

--- randomQuoteMachine/js/quoter.js
+++ randomQuoteMachine/js/quoter.js
@@ -1,6 +1,44 @@
-var body = document.getElementsByTagName("body")[0];
-var colors = ['LightSteelBlue', 'LightSeaGreen', 'goldenrod', 'magenta', 'orchid', 'palevioletred'];
+var quotes = [
+    {
+        "quote":"We must be willing to get rid of the life we've planned, so as to have the life that is waiting for us",
+        "author":"Joseph Campbell"
+    },
+    {
+        "quote":"Follow your bliss and the universe will open doors for you where there were only walls.",
+        "author":"Joseph Campbell"
+    },
+    {
+        "quote":"The cave you fear to enter holds the treasure you seek.",
+        "author":"Joseph Campbell"
+    }
+];
 
-function randBGColor() {
-    body.style.backgroundColor = colors[Math.floor(Math.random() * (colors.length - 1))];
+$(document).ready(function() {
+    $("#newQuote").on("click", function(){
+        var index = Math.floor(Math.random() * quotes.length);
+        var quote = quotes[index].quote;
+        var author = quotes[index].author;
+        var authorHash = hashtagFromName(author);
+        $('#quote-text').html(quote);
+        $('#quote-author').html(author);
+        $('.twitter-share-button').remove();
+        newTweetButton(quote, authorHash);
+        // $('.twitter-share-button').attr('data-text', quote);
+        // $('.twitter-share-button').attr('data-hashtags', authorHash);
+        twttr.widgets.load();
+        // $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + quotes[index].author + ':%20' + quotes[index].quote);
+    });
+});
+
+function hashtagFromName(name) {
+    var hash = name.split(' ').join('');
+    return hash;
+}
+function newTweetButton(text, hashtag){
+    $('<a></a>').addClass('twitter-share-button')
+                .attr('href', 'https://twitter.com/share')
+                .attr('data-size', 'small')
+                .attr('data-text', text)
+                .attr('data-hashtags', hashtag)
+                .html('Tweet').prependTo("#quote-container");
 }
