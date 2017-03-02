2fa8bdc29dd9d0a408cc9f288c9c3114e929fb21
Curt D Miller <curtdmiller@gmail.com>
2017-03-02 13:59:47 -0500

    created custom twitter button to prevent having to create new twitter buttons/call external twitter script every new quote

--- randomQuoteMachine/js/quoter.js
+++ randomQuoteMachine/js/quoter.js
@@ -1,6 +1,6 @@
 var quotes = [
     {
-        "quote":"We must be willing to get rid of the life we've planned, so as to have the life that is waiting for us",
+        "quote":"We must be willing to get rid of the life we've planned, so as to have the life that is waiting for us.",
         "author":"Joseph Campbell"
     },
     {
@@ -14,31 +14,20 @@ var quotes = [
 ];
 
 $(document).ready(function() {
-    $("#newQuote").on("click", function(){
-        var index = Math.floor(Math.random() * quotes.length);
-        var quote = quotes[index].quote;
-        var author = quotes[index].author;
-        var authorHash = hashtagFromName(author);
-        $('#quote-text').html(quote);
-        $('#quote-author').html(author);
-        $('.twitter-share-button').remove();
-        newTweetButton(quote, authorHash);
-        // $('.twitter-share-button').attr('data-text', quote);
-        // $('.twitter-share-button').attr('data-hashtags', authorHash);
-        twttr.widgets.load();
-        // $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + quotes[index].author + ':%20' + quotes[index].quote);
-    });
+    $("#newQuote").on("click", newQuote));
 });
 
 function hashtagFromName(name) {
     var hash = name.split(' ').join('');
     return hash;
 }
-function newTweetButton(text, hashtag){
-    $('<a></a>').addClass('twitter-share-button')
-                .attr('href', 'https://twitter.com/share')
-                .attr('data-size', 'small')
-                .attr('data-text', text)
-                .attr('data-hashtags', hashtag)
-                .html('Tweet').prependTo("#quote-container");
+
+function newQuote() {
+    var index = Math.floor(Math.random() * quotes.length);
+    var quote = quotes[index].quote;
+    var author = quotes[index].author;
+    var authorHash = hashtagFromName(author);
+    $('#quote-text').html(quote);
+    $('#quote-author').html(author);
+    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + quote + '&hashtags=' + authorHash);
 }
