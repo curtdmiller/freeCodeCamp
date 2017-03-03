a63812abb55a80fcd0ab98a06ed0f41f950a2883
Curt D Miller <curtdmiller@gmail.com>
2017-03-03 12:13:20 -0500

    use quotes on design api to fetch random quotes

--- randomQuoteMachine/js/quoter.js
+++ randomQuoteMachine/js/quoter.js
@@ -1,19 +1,5 @@
-var quotes = [
-    {
-        "quote":"We must be willing to get rid of the life we've planned, so as to have the life that is waiting for us.",
-        "author":"Joseph Campbell"
-    },
-    {
-        "quote":"Follow your bliss and the universe will open doors for you where there were only walls.",
-        "author":"Joseph Campbell"
-    },
-    {
-        "quote":"The cave you fear to enter holds the treasure you seek.",
-        "author":"Joseph Campbell"
-    }
-];
-
 $(document).ready(function() {
+    $.ajaxSetup({ cache: false }); // disable cache for getJSON calls
     firstQuote();
     $("#newQuote").on("click", newQuote);
 });
@@ -23,23 +9,26 @@ function hashtagFromName(name) {
     return hash;
 }
 function firstQuote() {
-    var index = Math.floor(Math.random() * quotes.length);
-    var quote = quotes[index].quote;
-    var author = quotes[index].author;
-    var authorHash = hashtagFromName(author);
-
-    $('blockquote').html('<p>' + quote + '</p><footer>' + author + '</footer>');
-    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text="' + quote + '"&hashtags=' + authorHash);
+    var quote, author, authorHash;
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
+        quote = json[0].content;
+        author = json[0].title;
+        authorHash = hashtagFromName(author);
+        // quote is a string of html with a p tag, can be added directly to the blockquote but needs parsing for the tweet
+        $('blockquote').html(quote + '<footer>' + author + '</footer>');
+        $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text="' + $.parseHTML(quote)[0].innerHTML + '"&hashtags=' + authorHash);
+    });
 }
 function newQuote() {
-    var index = Math.floor(Math.random() * quotes.length);
-    var quote = quotes[index].quote;
-    var author = quotes[index].author;
-    var authorHash = hashtagFromName(author);
-
-    $('blockquote').fadeOut('slow', function(){
-        $('blockquote').html('<p>' + quote + '</p><footer>' + author + '</footer>');
-        $('blockquote').fadeIn('slow');
-    }); // toggle display of quote divs
-    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text="' + quote + '"&hashtags=' + authorHash);
+    var quote, author, authorHash;
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
+        quote = json[0].content;
+        author = json[0].title;
+        authorHash = hashtagFromName(author);
+        $('blockquote').fadeOut('slow', function(){
+            $('blockquote').html(quote + '<footer>' + author + '</footer>');
+            $('blockquote').fadeIn('slow');
+        }); // toggle display of quote divs
+        $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text="' + $.parseHTML(quote)[0].innerHTML + '"&hashtags=' + authorHash);
+    });
 }
