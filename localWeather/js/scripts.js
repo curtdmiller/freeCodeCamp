d6ab6d3c64266eb199f488e81fd18ae6ba91362c
Curt D Miller <curtdmiller@gmail.com>
2017-03-05 20:56:36 -0500

    style weather data, add svg icons corresponding to current conditions, add readme

--- localWeather/js/scripts.js
+++ localWeather/js/scripts.js
@@ -1,25 +1,106 @@
 var weatherKey = config.WEATHER_KEY;
 
-var city, temp, humidity, conditions;
+var city, tempFahr, tempCel, conditions, icon;
 $(document).ready(function(){
     if ("geolocation" in navigator) {
-        /* geolocation is available */
-        $('.location').html('<p>getting location...</p>');
         navigator.geolocation.getCurrentPosition(function(position) {
             var latitude = position.coords.latitude;
             var longitude = position.coords.longitude;
             var apiURL = "//api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + "&APPID=" + weatherKey ;
-            $('.location').html('<p>getting weather...</p>');
+            $('.pre-load p').html('fetching weather')
             $.getJSON(apiURL, function(weather) {
-                console.log(weather);
+                $('.pre-load').css('display', 'none');
                 city = weather.name;
-                temp = weather.main.temp;
-                humidity = weather.main.humidity;
+                tempFahr = weather.main.temp;
+                tempCel = toCelsius(tempFahr);
                 conditions = weather.weather[0].main;
-                $('.location').html('<p>current weather for ' + city + ':<br>' + parseInt(temp) + '&deg;F<br>' + conditions + '<br>humidity:' + humidity + '%</p>');
+                icon = weatherIcon(conditions);
+                $('#location').html(city);
+                $('#conditions').html(conditions + ' | ' + parseInt(tempFahr) + '&deg;F');
+                $('#icon').html('<img src="' + icon + '">');
+                $('#unit-btn-group').css('display', 'block')
             });
         });
-    } else {
+    } else  {
         /* geolocation IS NOT available */
     }
-})
+    $('#cel').on('click', function() {
+        $('#conditions').html(conditions + ' | ' + parseInt(tempCel) + '&deg;C');
+        $('#cel').attr('disabled', 'disabled');
+        $('#fahr').removeAttr('disabled');
+    })
+    $('#fahr').on('click', function() {
+        $('#conditions').html(conditions + ' | ' + parseInt(tempFahr) + '&deg;F');
+        $('#cel').removeAttr('disabled');
+        $('#fahr').attr('disabled', 'disabled');
+    })
+});
+
+// Icons by Adam Whitcroft: http://adamwhitcroft.com/climacons/
+function weatherIcon(currentConditions) {
+    var d = new Date();
+    var h = d.getHours();
+    currentConditions = currentConditions.toLowerCase();
+    if(h > 7 && h < 19) { // if time is between 7am and 7pm
+        /* daytime icons */
+        switch (currentConditions) {
+            case 'clear':
+            case 'clear sky':
+                return 'svg/Sun.svg'
+                break;
+            case 'few clouds':
+            case 'scattered clouds':
+            case 'broken clouds':
+                return 'svg/Cloud-Sun.svg';
+                break;
+            case 'shower rain':
+                return 'svg/Cloud-Drizzle-Sun.svg';
+                break;
+            case 'rain':
+                return 'svg/Cloud-Rain.svg';
+                break;
+            case 'thunderstorm':
+                return 'svg/Cloud-Lightning.svg';
+                break;
+            case 'snow':
+                return 'svg/Cloud-Snow.svg';
+                break;
+            case 'mist':
+                return 'svg/Cloud-Fog.svg';
+                break;
+            default:
+        }
+    } else {
+        /* nighttime icons */
+        switch (currentConditions) {
+            case 'clear':
+            case 'clear sky':
+                return('svg/Moon.svg');
+                break;
+            case 'few clouds':
+            case 'scattered clouds':
+            case 'broken clouds':
+                return 'svg/Cloud-Moon.svg';
+                break;
+            case 'shower rain':
+                return 'svg/Cloud-Drizzle-Moon.svg';
+                break;
+            case 'rain':
+                return 'svg/Cloud-Rain.svg';
+                break;
+            case 'thunderstorm':
+                return 'svg/Cloud-Lightning.svg';
+                break;
+            case 'snow':
+                return 'svg/Cloud-Snow.svg';
+                break;
+            case 'mist':
+                return 'svg/Cloud-Fog.svg';
+                break;
+            default:
+        }
+    }
+}
+function toCelsius(degFahr) {
+    return ((degFahr - 32) * 5) / 9;
+}
