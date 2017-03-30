0c928664aa61477b0b6705c55f92a691460e8912
Curt D Miller <curtdmiller@gmail.com>
2017-03-29 21:20:24 -0400

    fix refresh button width, refactor ids into classes, change background color

--- randomQuoteMachine/js/quoter.js
+++ randomQuoteMachine/js/quoter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.ajaxSetup({ cache: false }); // disable cache for getJSON calls
     firstQuote();
-    $("#refreshBtn").on("click", newQuote);
+    $(".refresh-btn").on("click", newQuote);
 });
 
 function hashtagFromName(name) {
@@ -16,22 +16,22 @@ function firstQuote() {
         authorHash = hashtagFromName(author);
         // quote is a string of html with a p tag, can be added directly to the blockquote but needs parsing for the tweet
         $('blockquote').html(quote + '<footer>' + author + '</footer>');
-        $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text="' + $.parseHTML(quote)[0].innerHTML + '"&hashtags=' + authorHash);
+        $('.tweet-btn').attr('href', 'https://twitter.com/intent/tweet?text="' + $.parseHTML(quote)[0].innerHTML + '"&hashtags=' + authorHash);
     });
 }
 function newQuote() {
     var quote, author, authorHash;
     // change refresh button to spinner until ajax operation complete
-    $('#refreshBtn').html('<i class="fa fa-spinner fa-lg fa-spin"></i>');
+    $('.refresh-btn').html('<i class="fa fa-spinner fa-lg fa-spin"></i>');
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
         quote = json[0].content;
         author = json[0].title;
         authorHash = hashtagFromName(author);
-        $('#refreshBtn').html('<i class="fa fa-refresh fa-lg"></i>');
+        $('.refresh-btn').html('<i class="fa fa-refresh fa-lg"></i>');
         $('blockquote').fadeOut('slow', function(){
             $('blockquote').html(quote + '<footer>' + author + '</footer>');
             $('blockquote').fadeIn('slow');
         }); // toggle display of quote divs
-        $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text="' + $.parseHTML(quote)[0].innerHTML + '"&hashtags=' + authorHash);
+        $('.tweet-btn').attr('href', 'https://twitter.com/intent/tweet?text="' + $.parseHTML(quote)[0].innerHTML + '"&hashtags=' + authorHash);
     });
 }
