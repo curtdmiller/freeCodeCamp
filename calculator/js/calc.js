6f689ce351a2e88b6d6ab1618f1a67bfa4d9863a
Curt D Miller <curtdmiller@gmail.com>
2017-04-08 11:52:06 -0400

    implement use of big.js for far better floating point math, clean up resultant unnecessary code.

--- calculator/js/calc.js
+++ calculator/js/calc.js
@@ -1,20 +1,22 @@
+// using big.js for ease of floating point math
+Big.DP = 5; // big.js max decimal places for division and sqrt methods
+Big.RM = 1; // big.js rounding mode, Rounds towards nearest neighbour. If equidistant, rounds away from zero.
+
 var num = ''; // currently displayed number
 var expr = []; // complete pre equal expression
 var curExpr = ''; // current string version of the expression
 var isDec = false; // current number is decimal?
-var dec = 0; // current number of digits after decimal point
-var sigfig = 0; // sig figs for entire expression
 var result; // result of expression
 var isRollover = false; // is number the result of the last expression?
+
 $(document).ready(function(){
     $('.clear').click(function(){
         if (num) {
             curExpr = curExpr.slice(0, -num.length); // remove only the num entered at end of expr
         }
-        num = ""; // reset num
+        num = ''; // reset num
         $('.screen h3').text('0');
         $('.expr').text(curExpr);
-        dec = 0;
     });
     $('.all-clear').click(function(){
         expr = [];
@@ -23,18 +25,11 @@ $(document).ready(function(){
         $('.screen h3').text('0');
         $('.expr').text('');
         isDec = false;
-        dec = 0;
-        sigfig = 0;
     });
     $('.num').click(function(){
         if (isRollover) { // jQuery utility, if num is type Number, it is the result of the last operation, don't add to, clear and restart
             $('.all-clear').click();
             isRollover = false;
-        } else if (isDec) {
-            dec++; // if decimal, increase dec count
-            if (dec > sigfig) { // if the current number has more decimals than sig fig (all numbers in expression)
-                sigfig = dec;
-            }
         } else if (num === '0') {
             num = '';
             curExpr = curExpr.slice(0, -1);
@@ -58,14 +53,13 @@ $(document).ready(function(){
         }
     });
     $('.op').click(function(){
-        if (num !== ''){ // only push to expr if num has been entered and it is not a holdover result from last expression
+        if (num !== ''){ // only push to expr if num has been entered
             expr.push(num);
             expr.push($(this).attr('value')); // push operator into expression array
             num = '';
             curExpr += $(this).attr('value');
             $('.expr').text(curExpr);
             isDec = false; // reset isDec tracker
-            dec = 0; // reset current number decimal count
             isRollover = false;
         }
     });
@@ -78,13 +72,8 @@ $(document).ready(function(){
         }
         if (expr.length === 1) result = expr[0]; // if only a value, result equals that value
         while(expr.length > 2){ // operate on groups of three array objects
-            var operation = operator(expr[1]); // create a math function based on operator
-            if (sigfig) {
-                result = operation(parseFloat(expr[0]), parseFloat(expr[2])); // use the returned function
-                result = result.toFixed(sigfig); // round to sig figs
-            } else {
-                result = operation(parseInt(expr[0]), parseInt(expr[2]));
-            }
+            var operation = opBig(expr[1]); // create a big.js math function based on operator
+            result = operation(expr[0],expr[2]);
             expr.splice(0,3); // remove the first three items
             expr.unshift(result); // add result to the beginning for the next operation
         }
@@ -97,23 +86,31 @@ $(document).ready(function(){
     });
 });
 
-function operator(op) {
+function opBig(op) {
     switch (op) {
         case '+':
-        return function(a,b){
-            return a + b;
+        return function(a,b) {
+            a = new Big(a);
+            b = new Big(b);
+            return a.plus(b).toString();
         }
         case '-':
         return function(a,b){
-            return a - b;
+            a = new Big(a);
+            b = new Big(b);
+            return a.sub(b).toString();
         }
         case '*':
         return function(a,b){
-            return a * b;
+            a = new Big(a);
+            b = new Big(b);
+            return a.mul(b).toString();
         }
         case '/':
         return function(a,b){
-            return a / b;
+            a = new Big(a);
+            b = new Big(b);
+            return a.div(b).toString();
         }
         default:
         console.log('error!');
